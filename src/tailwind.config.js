03bf730735b00ead0f4825bf514a701ae7da1862
majaara <M.arament@ofiservices.com>
2025-07-18 15:08:24 +0200

    Add Inter font and update Tailwind config with custom fontFamily

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,10 +1,8 @@
-// tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Scan all JS/TS/JSX/TSX files in src
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -12,6 +10,9 @@ module.exports = {
         "ofi-blue": "#1d263b",
         "ofi-dark": "#151d2d",
         "ofi-light": "#283144",
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'], // Registering the Inter font
       },
     },
   },
